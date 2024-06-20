7783a5bb13f632c817b1bfae453dabc3874616b7
Dominic Nunez <dominicnunez@gmail.com>
2024-06-20 16:05:50 -0500

    Feature: character card

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -7,12 +7,33 @@ const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
+  const [planets, setPlanets] = useState([])
+  const [people, setPeople] = useState([])
+
   // ❗ Create effects to fetch the data and put it in state
+  useEffect(() => {
+    Promise.all([
+      axios.get(urlPlanets),
+      axios.get(urlPeople)
+    ])
+      .then(([planetsResponse, peopleResponse]) => {
+      setPlanets(planetsResponse.data)
+      setPeople(peopleResponse.data)
+      })
+      .catch(err => console.error(err))
+  }, [])
+
+  // �� Render the data from state in the UI
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {/* ❗ Map over the data in state, rendering a Character at each iteration */
+        people.map((person) => {
+          return (
+            <Character key={person.id} person={person} planets={planets} />
+          )
+        })}
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
+function Character({person, planets}) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+  const [showHomeworld, setShowHomeworld] = useState(false)
+
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const toggleHomeworld = () => {
+    setShowHomeworld(!showHomeworld)
+  }
+
   return (
-    <div>
+    <div className='character-card' onClick={toggleHomeworld}>
+      <h3 className='character-name'>{person.name}</h3>
+      { showHomeworld && (
+        <p>Planet: 
+          <span className='character-planet'> 
+          { planets.find(( planet ) => planet.id === person.homeworld)?.name }
+          </span>
+        </p>
+      )}
+      
       {/* Use the same markup with the same attributes as in the mock */}
+      <style>
+        {`
+          .character-planet {
+            margin-left: 4px;
+          }
+        `}
+      </style>
     </div>
   )
 }
